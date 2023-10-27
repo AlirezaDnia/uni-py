d61731c8f9ada8ab5f51658ec43ffa6771842b53
AlirezaDnia <a.davoodinia@gmail.com>
2023-10-27 23:55:56 +0330

    first commit

---
+++ ai/math.py
@@ -0,0 +1,27 @@
+#input numbers
+a = int(input("Please Enter Your num1: "))
+b = int(input("Please Enter Your num2: "))
+
+#1
+Parentheses = (a+b)*a
+print(f"Parentheses(({a}+{b})*{a}):" , Parentheses)
+
+#2
+Exponent = a**b
+print(f"Exponent({a}^{b}):" , Exponent)
+
+#3
+plus = a+b
+print(f"plus({a}+{b}):" , plus)
+
+#4
+minus = a-b
+print(f"minus({a}-{b}):" , minus)
+
+#5
+Multiplication = a*b
+print(f"Multiplication({a}*{b}):" , Multiplication)
+
+#6
+Division = a/b
+print(f"Division({a}/{b}):" , Division)
