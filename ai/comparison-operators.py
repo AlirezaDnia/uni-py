b0decc89701b4395ee755536478eb2e8c410fd6f
AlirezaDnia <a.davoodinia@gmail.com>
2023-11-10 23:49:55 +0330

    add comparison operators

---
+++ ai/comparison-operators.py
@@ -0,0 +1,22 @@
+a = 10;
+b = 20;
+
+print("welcome to my prog")
+
+if ( a == b ):
+   print("a == b");
+
+if( a != b ):
+   print("a != b");
+
+if( a > b ):
+   print("a > b");
+
+if( a < b ):
+   print("a < b");
+
+if( a >= b ):
+   print("a >= b");
+
+if( a <= b ):
+   print("a <= b");
